1059a6d9189c7651dc9c685849c867a48970f834
Kevin <brackkevin@gmail.com>
2018-07-27 10:58:48 -0500

    Add PUT: method to update project

--- index.js
+++ index.js
@@ -59,6 +59,20 @@ server.post("/api/projects", async (req, res) => {
   }
 });
 
+server.put("/api/projects/:id", async (req, res) => {
+  try {
+    // TODO: add validation
+    const id = req.params.id;
+    const changes = req.body;
+    const response = await projectModel.update(id, changes);
+    res.status(200).send(response);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "ERROR PROCESSING REQUEST", error: error.message });
+  }
+});
+
 // actions
 
 // catch all 404
