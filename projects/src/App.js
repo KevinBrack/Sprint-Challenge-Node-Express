7c636612cf3b2b31dd1ac28148ed3140b34f4703
Kevin <brackkevin@gmail.com>
2018-07-27 12:16:36 -0500

    Move components into src folder in projects app

--- projects/src/App.js
+++ projects/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import axios from "axios";
+import Header from "../components/Header/Header";
 
 class App extends Component {
   constructor(props) {
@@ -22,7 +23,11 @@ class App extends Component {
   };
 
   render() {
-    return <div className="App">APP</div>;
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
   }
 }
 
---
+++ projects/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Sprint Challenge Node Express</h1>
+      <h2>By Kevin Brack</h2>
+    </div>
+  );
+};
+
+export default Header;
